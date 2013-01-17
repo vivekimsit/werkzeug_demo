3bdfec6f497f66a6f8f91b6e9a22e9f251aadf82
vivekimsit <vivekimsit@gmail.com>
2013-01-17 20:15:43 +0530

    Transformed into an Blog app.

--- simple_server.py
+++ simple_server.py
@@ -1,32 +1,74 @@
 # -*- coding: utf-8 -*- 
-from werkzeug.wrappers import Response
+import os
+from werkzeug.wrappers import Request, Response
+from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
+from werkzeug.exceptions import HTTPException, NotFound
+from werkzeug.wsgi import SharedDataMiddleware
 
-def application(env, start_response):
-    response = Response("Hello World!", mimetype='text/plain')
-    return response(env, start_response)
+from jinja2 import Environment, FileSystemLoader
 
-#from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
-#
-#url_map = Map([
-#        Rule('/', endpoint='blog/archive'),
-#        Rule('/<int:year>/', endpoint='blog/archive'),
-#        Rule('/<int:year>/<int:month>/<slug>', endpoint='blog/show_post'),
-#        Rule('/feeds/<feed_name>.rss', endpoint='blog/show_feed')
-#        ])
-#
-#def application(env, start_res):
-#    urls = url_map.bind_to_environ(env) # Returns the MapAdapter object.
-#    try:
-#        endpoint, args = urls.match()
-#    except HTTPException, e:
-#        return e(env, start_res)
-#    start_res = ('200 OK', [('Content-Type', 'text/plain')])
-#    return ['Rule points to %r with arguments %r'%(endpoint, args)]
+HOST = 'localhost'
+PORT = 5000
+STATIC_FOLDER = 'static'
+TEMPLATE_FOLDER = 'templates'
+#def application(env, start_response):
+#    response = Response("Hello World!", mimetype='text/plain')
+#    return response(env, start_response)
+
+class Blog(object):
+    """
+    A simple blog application class.
+    """
+    def __init__(self):
+        self.url_map = Map([
+            Rule('/', endpoint='home'),
+            Rule('/about/', endpoint='about'),
+            Rule('/projects', endpoint='projects'),
+            Rule('/posts/<int:year>/<int:month>/<int:date>', endpoint='show_post')
+            ])
+        # Add support for the Templates.
+        template_path = os.path.join(
+                            os.path.dirname(__file__), 
+                            TEMPLATE_FOLDER)
+        self.jinja_env = Environment(loader=FileSystemLoader(template_path))
+
+    def __call__(self, env, start_response):
+        return self.application(env, start_response)
+
+    def render_template(self, template_name, **context):
+        templ = self.jinja_env.get_template(template_name)
+        return Response(templ.render(context), mimetype='text/html')
+
+    def view_home(self, req, args):
+        return self.render_template('home.html')
+        #return Response("You are in home :)")
+
+    def view_about(self, req, args):
+        return Response("You are in about :)")
+
+    # The main application callable, it will be called on each request. 
+    def application(self, env, start_response):
+        request = Request(env)
+        urls = self.url_map.bind_to_environ(env) # Returns the MapAdapter object.
+        try:
+            endpoint, args = urls.match()
+        except HTTPException, e:
+            return e(env, start_response)
+        #start_response('200 OK', [('Content-Type', 'text/plain')])
+        response = getattr(self, 'view_' + endpoint)(request, args)
+        return response(env, start_response)
+        #return ['Rule points to %r with arguments %r'%(endpoint, args)]
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
+    app = Blog()
+    static_path = os.path.join(os.path.dirname(__file__), STATIC_FOLDER)
+    # Apply a wrapper around the callable application.
+    #app.application = SharedDataMiddleware(
+    #                        app.application, 
+    #                        {
+    #                            '/static': static_path, 
+    #                        })
     run_simple(
-        'localhost', 5000, application, use_debugger=True,
+        HOST, PORT, app, use_debugger=True,
         use_reloader=True)
-
-
